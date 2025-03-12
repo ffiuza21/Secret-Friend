ac37069c1d0a4fcf5a571a8dd3296430c36e6c7f
ffiuza <ffiuzasantos@gmail.com>
2025-03-12 13:35:42 -0300

    creating the logic to drawn random but different friends

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let friendsListField = document.getElementById('friends-list');
 let sorting = document.getElementById('sorting-list');
 
 function add() {
+ 
     let friendsName = document.getElementById('friends-name').value;
     
     if (friendsName == '') {
@@ -13,17 +14,37 @@ function add() {
     friendsList.push(friendsName);
     friendsListField.innerText = friendsList;
 
+
+    
     // add clear field when a name is added
     // add spaces between names on friend's list field
 }
 
-function draw() {
+drawn = [];
+
+function randomDrawn() {
+    
     for (let i = 0; i < friendsList.length; i++) {
-        sorting.innerHTML += `<p>${friendsList[i]} ---> </p>`;
+        let sorteado = friendsList[parseInt(Math.random() * friendsList.length)];
+        if (drawn.includes(sorteado)) {
+            friendsList[parseInt(Math.random() * friendsList.length)];
+        } else {
+            drawn.push(sorteado);
+        }
+    }
+}
+// loop nao esta sorteando valored diferentes do que ja esta no array drawn
+
+
+function draw() {
+    
+randomDrawn();
+
+    for (let i = 0; i < friendsList.length; i++) {
+        sorting.innerHTML += `<p>${friendsList[i]} ---> ${drawn[i]}</p>`;
     }
 
-    
-
+   
 
 
 
