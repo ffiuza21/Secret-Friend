bdb8d2ae29b869b89210883576ae5a26298146d3
ffiuza2 <ffiuzasantos@gmail.com>
2025-03-13 18:38:45 -0300

    Finished Secret Friend Game

--- js/app.js
+++ js/app.js
@@ -1,64 +1,64 @@
-friendsList = [];
+let friendsList = [];
+
 let friendsListField = document.getElementById('friends-list');
+
 let sorting = document.getElementById('sorting-list');
 
 function add() {
  
     let friendsName = document.getElementById('friends-name').value;
+
     let friendsNameField = document.getElementById('friends-name');
 
     if (friendsName == '') {
+
         alert('Please type a name');
+
+        return;
+    }
+
+    if (friendsList.includes(friendsName)) {
+
+        alert('The name was already added');
+
         return;
     }
 
     friendsList.push(friendsName);
+
     friendsListField.innerText = friendsList;
+
     friendsNameField.value = '';
 }
 
-drawn = [];
-
-function randomDrawn() {
-    
-    for (let i = 0; i < friendsList.length; i++) {
-        let sorteado = friendsList[parseInt(Math.random() * friendsList.length)];
-        if (!drawn.includes(sorteado)) {
-        drawn.push(sorteado);
-        }else {
-            friendsList[parseInt(Math.random() * friendsList.length)];
-        }
-           
-    }
-}
-// loop nao esta sorteando valored diferentes do que ja esta no array drawn
-
-
 function draw() {
     
-randomDrawn();
+    if (friendsList.length < 4) {
 
-    for (let i = 0; i < friendsList.length; i++) {
-        sorting.innerHTML += `<p>${friendsList[i]} ---> ${drawn[i]}</p>`;
+        alert('Please add at least 4 friends to draw');
+
+        return;
     }
 
-   
 
+    for (let i = 0; i < friendsList.length; i++) {
 
+        if (i == friendsList.length - 1) {
 
+            sorting.innerHTML += friendsList[i] + '--->' + friendsList[0] + '<br>';
+
+        } else {
+
+            sorting.innerHTML += friendsList[i] + '--->' + friendsList[i + 1] + '<br>';
+        }
+    }
 }
 
-
-
-
 function restart() {
+
     friendsList = [];
+
     friendsListField.innerText = '';
 
-
-
-
-
-
-
+    sorting.innerHTML = '';
 }
